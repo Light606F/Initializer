0ea0c4d392563220caf8cdaa2d0465a950073e5e
Light606F <Light606F.tsukineko@gmail.com>
2018-10-01 16:25:22 +0900

    Change install process for Atom.

--- installAtom.bash
+++ installAtom.bash
@@ -3,7 +3,14 @@ echo "Install atom"
 echo -e "\tWORNING to install atom. add-apt-repositiory is duplicatable."
 read -p "Hit enter: "
 
-sudo add-apt-repository ppa:webupd8team/atom
+# old?
+# sudo add-apt-repository ppa:webupd8team/atom
+# sudo apt update
+# sudo apt install atom
+
+# official
+curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt update
 sudo apt install atom
 
