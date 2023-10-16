9303998c0052504ba88bd20d72f7f57db09935cd
Light606F <Light606F.tsukineko@gmail.com>
2023-10-17 01:57:08 +0900

    change atom to vscode

--- initializeUbuntu.bash
+++ initializeUbuntu.bash
@@ -13,6 +13,11 @@ LANG=C xdg-user-dirs-gtk-update
 # ---vvv---日本語化してあるなら実行する?とこ
 # mozc の設定画面が開く．
 echo "Set mozc setting."
+echo "モード	入力キー	コマンド
+直接入力	Ctrl Space	IMEを有効化
+入力文字なし	Ctrl Space	IMEを無効化
+変換前入力中	Ctrl Space	IMEを無効化
+変換中	Ctrl Space	IMEを無効化"
 read -p "Hit enter: "
 /usr/lib/mozc/mozc_tool -mode=config_dialog
 # ---^^^---日本語化してあるなら実行する?とこ
@@ -46,12 +51,12 @@ while true; do
     esac
 done
 
-# Install atom
-echo "Install atom"
+# Install VSCode
+echo "Install VSCode"
 while true; do
     read -p "Do you wish to install this program?" yn
     case $yn in
-        [Yy]* ) bash installAtom.bash; break;;
+        [Yy]* ) sudo snap install --classic code; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
